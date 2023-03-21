03659783df96f2f59f91717d7a9d8867e6a273da
FirestormPenguin <edoardogiubellini@gmail.com>
2023-03-21 18:12:55 +0100

    commit

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: egiubell <egiubell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:35:46 by egiubell          #+#    #+#             */
-/*   Updated: 2023/03/21 16:25:02 by egiubell         ###   ########.fr       */
+/*   Updated: 2023/03/21 17:15:19 by egiubell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,20 @@
 
 int main ()
 {
+	t_stack *head;
 	t_stack vars;
 
 	vars.a = 1;
+	head->a = 32;
+	head->next  = ft_new_node();
+	head = head->next;
+
 	
+	while( != NULL)
+	{
+		printf ("A:\n%d\n", vars.a);
+		printf ("B:\n%d\n", vars.b);
+	}
 	
-	printf ("A:\n%d\n", vars.a);
-	printf ("B:\n%d\n", vars.b);
 	return (0);
 }
