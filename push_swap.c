dbc64c63eea5af5b3988be6c101781fd38e4dfb9
FirestormPenguin <edoardogiubellini@gmail.com>
2023-03-30 17:49:14 +0200

    commit

--- push_swap.c
+++ push_swap.c
@@ -6,46 +6,54 @@
 /*   By: egiubell <egiubell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:35:46 by egiubell          #+#    #+#             */
-/*   Updated: 2023/03/28 18:57:19 by egiubell         ###   ########.fr       */
+/*   Updated: 2023/03/30 17:47:36 by egiubell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int test(t_stack *stack)
+{
+	stack->count_a = stack->count_a - 1;
+	printf("%d\n", stack->count_a);
+	return (0);
+}
+
 int main (int ac, char **av)
 {
-	int	*stack_a;
-	int	*stack_b;
+	t_stack		*stack;
 	int	i;
 	int	j;
-	int	count_a;
 
 	i = 1;
 	j = 0;
-	count_a = ft_count_av(av);
-	printf("Numeri inseriti nella stack: %d\n\n", count_a);
+	stack = malloc(sizeof(t_stack));
+	stack->count_a = ft_count_av(av);
+	printf("Numeri inseriti nella stack: %d\n\n", stack->count_a);
 	
 	if(ac <= 2)
 	{
 		return (0);
 	}
-	stack_a = malloc(sizeof(int) * count_a);
-	stack_b = ft_calloc(sizeof(int), count_a);
+	stack->stack_a = malloc(sizeof(int) * stack->count_a);
+	stack->stack_b = malloc(sizeof(int) * stack->count_a);
 
 	while(av[i] != '\0')
 	{
-		stack_a[j] = ft_atoi(av[i]);
+		stack->stack_a[j] = ft_atoi(av[i]);
 		i++;
 		j++;
 	}
 	j = 0;
-	while(j < count_a)
+	while(j < stack->count_a)
 	{
-		printf("%d	%d\n", stack_a[j], stack_b[j]);
+		printf("%d	 \n", stack->stack_a[j]);
 		j++;
 	}
 	printf("-	-\n");
 	printf("A:	B:\n");
+	
+	test(stack);
 
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct s_stack {
+	int	*stack_a;
+	int	*stack_b;
+	int	count_a;
+	int	count_b;
+	int	current_a;
+	int	current_b;
+}		t_stack;
+
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 int		ft_atoi(const char *str);
-int	ft_count_av(char **str);
+int		ft_count_av(char **str);
